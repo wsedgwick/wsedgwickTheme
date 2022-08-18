89c68d47db9303ec71509fe8373d17fc73d88139
wsedgwick <wsedgwick0@gmail.com>
2022-08-18 15:27:49 -0700

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,31 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+theme_buta <- function() {
+  theme(
+    panel.background = element_rect(fill = "yellow"),
+    panel.grid.major.x = element_line(colour = "purple", linetype = 3, size = 0.5),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y =  element_line(colour = "cyan4", linetype = 3, size = 0.5),
+    axis.text = element_text(colour = "red"),
+    axis.title = element_text(colour = "orange")
+  )
+}
